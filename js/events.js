11651d69eec007bf65c833a47a1ffbf16879888d
Kevin Evans <mkevinevans@gmail.com>
2019-10-15 17:01:55 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,24 @@
 //define functions here
+function getIt(){
+  $('p').on('click', ()=>alert("Hey!"))
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass("tasty")
+  })
+}
+
+function pressIt(){
+  $('#typing').on('keydown', (e)=>{
+    if(e.which === 71 || 103){
+      alert("g pressed")
+    }
+  })
+}
 
 $(document).ready(function(){
 
-// call functions here
 
 });
+
