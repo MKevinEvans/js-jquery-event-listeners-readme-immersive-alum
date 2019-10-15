7018f74a9d941f52445f37d561bf71aea966eb47
Kevin Evans <mkevinevans@gmail.com>
2019-10-15 17:04:59 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -18,7 +18,7 @@ function pressIt(){
 }
 
 function submitIt(){
-  $('form').on('submit', function(){
+  $('#typing').on('submit', function(){
     alert("Your form is going to be submitted now.")
   })
 }
